493ba545b783c111d440440f689cceb94971ab09
Joey Maassen <joseph.maassen@gmail.com>
2013-06-25 13:35:55 -0500

    fixing asset pipeline

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application.
 TicTacToe::Application.initialize!
-
-config.assets.precompile += %w( application.js )
-config.assets.precompile += %w( application.css )
